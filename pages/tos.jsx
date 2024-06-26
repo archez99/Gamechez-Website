4ec9c27c1786351ea28c47396491ac9a7596ebe8
archez99 <138931233+archez99@users.noreply.github.com>
2024-06-27 02:13:35 +0300

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -4,17 +4,17 @@ export default function Tos() {
      <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
       <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ Every discord server can use the Gamechez bot for free, it is completely prohibited to sell or market for a fee.</p>
+<p>⊳ Her Discord sunucusu, Gamechez botunu ücretsiz kullanabilir, satmak veya ücret karşılığında pazarlamak kesinlikle yasaktır.</p>
 <div className="py-3"></div>
-<p>⊳ You cannot use bot commands to harm the bot.</p>
+<p>⊳ Bot komutlarını bota zarar vermek için kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ You may not use open source codes to harm other servers.</p>
+<p>⊳ Açık kaynak kodlarını diğer sunuculara zarar verecek şekilde kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ You may not sell or market open source codes through other platforms.</p>
+<p>⊳ Açık kaynak kodlarını başka platformlar üzerinden satamaz veya pazarlayamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ While using the bot, you cannot use it in a way that harms other users for troll purposes.</p>
+<p>⊳ Botu kullanırken trol amaçlı, diğer kullanıcılara zarar verecek şekilde kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ To use certain discord bot commands, you need to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">Gamechez</a> bot at certain intervals.</p>
+<p>⊳ Belirli discord bot komutlarını kullanmak için belirli aralıklarla  <a className="text-white" href="https://top.gg/bot/1158516555546628126/vote">Gamechez</a> botuna oy vermelisiniz.</p>
 </div>
     </div>
   );
