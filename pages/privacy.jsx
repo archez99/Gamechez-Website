dbb9d9c95c06e69d4bb11c3839aa55d4cc91c2e0
archez99 <138931233+archez99@users.noreply.github.com>
2024-06-27 02:10:52 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -5,7 +5,7 @@ export default function Privacy() {
             <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Gamechez</h1>
             <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ Gamechez botunu kullanmak için discord sunucunuza eklerken gerekli izinleri tanımlamanız gerekmektedir. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+        <p>⊳ Gamechez'i sunucusuna ekleyen herkes bu politikaları kabul etmiş sayılır.</p>
         <div className="py-3"></div>
         <p>⊳ Bazı Komutları Kullanırken <a className="text-white" href="https://top.gg/bot/1158516555546628126/vote">Gamechez</a> bota Oy Vermen Gerekmektedir.</p>
         <div className="py-3"></div>
