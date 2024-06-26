9fdc88eb1552fc0ebb50d9642160dea720667ac2
archez99 <138931233+archez99@users.noreply.github.com>
2024-06-27 02:08:12 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -5,17 +5,17 @@ export default function Privacy() {
             <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Gamechez</h1>
             <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ To use the Gamechez bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+        <p>⊳ Gamechez botunu kullanmak için discord sunucunuza eklerken gerekli izinleri tanımlamanız gerekmektedir. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
         <div className="py-3"></div>
-        <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">Gamechez</a> bot when using certain commands from the bot commands.</p>
+        <p>⊳ Bazı Komutları Kullanırken <a className="text-white" href="https://top.gg/bot/1158516555546628126/vote">Gamechez</a> bota Oy Vermen Gerekmektedir.</p>
         <div className="py-3"></div>
-        <p>⊳ Every user can use the Gamechez bot however they want, as long as they do not intend to harm.</p>
+        <p>⊳ Her kullanıcı Gamechez botunu zarar verme amacı taşımadığı sürece dilediği gibi kullanabilir.</p>
         <div className="py-3"></div>
-        <p>⊳ You cannot use certain commands at the same time while using Gamechez.</p>   
+        <p>⊳ Gamechez'i kullanırken belirli komutları aynı anda kullanamazsınız.</p>   
         <div className="py-3"></div>
-        <p>⊳ Gamechez bot does not save any data based on users.</p>
+        <p>⊳ Gamechez bot, kullanıcılara dayalı gerekli bilgileri kayıt altına alır!</p>
         <div className="py-3"></div>
-        <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
+        <p>⊳ Botu kullanırken kullanıcı kimliğiniz (ID) Kayıt Edilebilir.</p> 
     
         </div>
             </div>
