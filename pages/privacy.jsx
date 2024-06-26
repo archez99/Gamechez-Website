e22e3afcd321760fdeb21963ed07d9c254904dc5
archez99 <138931233+archez99@users.noreply.github.com>
2024-06-27 02:22:09 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -3,7 +3,7 @@ export default function Privacy() {
     return (
         <div>
             <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Gamechez</h1>
-            <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
+            <p className="text-sm text-white/30 mb-4">Last Update: 27/06/2024  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
         <p>⊳ Gamechez'i sunucusuna ekleyen herkes bu politikaları kabul etmiş sayılır.</p>
         <div className="py-3"></div>
