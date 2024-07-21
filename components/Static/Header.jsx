a9919fb2a32ed13058de0c58a0d796e6e2ac6d8b
archez99 <138931233+archez99@users.noreply.github.com>
2024-07-21 15:35:48 +0300

    Update Header.jsx

--- components/Static/Header.jsx
+++ components/Static/Header.jsx
@@ -168,7 +168,7 @@ const Header = ({ $, NavItems }) => {
                         </Menu>
                         <Link href="https://discord.com/oauth2/authorize?client_id=1158516555546628126&permissions=8&integration_type=0&scope=bot+applications.commands%20bot">
                             <a className="w-auto flex items-center justify-center shadow-lg gap-x-2 shadow-amber-600/20 rounded-xl py-2.5 font-medium px-7 bg-gradient-to-tl from-amber-500 to-amber-700 text-white  hover:opacity-80 transition duration-200">
-                               Davet Et
+                               Invite Bot
                             </a>
                </Link>
                     </div>
